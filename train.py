64104242daa1db88e0c5d738ecc62a48757f23e4
vis lee <vis_lee@msn.com>
2019-05-09 18:01:30 +0800

    add loop_all functionality for iteratively training

--- train.py
+++ train.py
@@ -10,7 +10,6 @@ from deepsleep.sleep_stage import (NUM_CLASSES,
                                    EPOCH_SEC_LEN,
                                    SAMPLING_RATE)
 
-
 FLAGS = tf.app.flags.FLAGS
 
 tf.app.flags.DEFINE_string('data_dir', 'data',
@@ -29,13 +28,17 @@ tf.app.flags.DEFINE_integer('finetune_epochs', 200,
 tf.app.flags.DEFINE_boolean('resume', False,
                             """Whether to resume the training process.""")
 
+tf.app.flags.DEFINE_boolean('loop_all', True,
+                            """Whether to train all subjects iteratively.""")
 
-def pretrain(n_epochs):
+
+
+def pretrain(n_epochs, fold_idx):
     trainer = DeepFeatureNetTrainer(
         data_dir=FLAGS.data_dir, 
         output_dir=FLAGS.output_dir,
         n_folds=FLAGS.n_folds, 
-        fold_idx=FLAGS.fold_idx,
+        fold_idx=fold_idx,
         batch_size=100, 
         input_dims=EPOCH_SEC_LEN*100, 
         n_classes=NUM_CLASSES,
@@ -50,12 +53,12 @@ def pretrain(n_epochs):
     return pretrained_model_path
 
 
-def finetune(model_path, n_epochs):
+def finetune(model_path, n_epochs, fold_idx):
     trainer = DeepSleepNetTrainer(
         data_dir=FLAGS.data_dir, 
         output_dir=FLAGS.output_dir, 
         n_folds=FLAGS.n_folds, 
-        fold_idx=FLAGS.fold_idx, 
+        fold_idx=fold_idx, 
         batch_size=10, 
         input_dims=EPOCH_SEC_LEN*100, 
         n_classes=NUM_CLASSES,
@@ -75,20 +78,38 @@ def finetune(model_path, n_epochs):
 
 
 def main(argv=None):
-    # Output dir
-    output_dir = os.path.join(FLAGS.output_dir, "fold{}".format(FLAGS.fold_idx))
-    if not FLAGS.resume:
-        if tf.gfile.Exists(output_dir):
-            tf.gfile.DeleteRecursively(output_dir)
-        tf.gfile.MakeDirs(output_dir)
 
-    pretrained_model_path = pretrain(
-        n_epochs=FLAGS.pretrain_epochs
-    )
-    finetuned_model_path = finetune(
-        model_path=pretrained_model_path, 
-        n_epochs=FLAGS.finetune_epochs
-    )
+    # if loop_all is True, n_folds used for loop range and fold_idx
+    # used for loop end index.
+    # if loop_all is False, fold_idx used to specify the training index
+
+    if FLAGS.loop_all is True:
+        n_folds = FLAGS.n_folds
+    else:
+        n_folds = 1
+
+    for i in range(n_folds):
+        # if not loop, fall back to fold_idx
+        if FLAGS.loop_all is False:
+            i = FLAGS.fold_idx
+
+        # Output dir
+        output_dir = os.path.join(FLAGS.output_dir, "fold{}".format(i))
+        if not FLAGS.resume:
+            if tf.gfile.Exists(output_dir):
+                tf.gfile.DeleteRecursively(output_dir)
+            tf.gfile.MakeDirs(output_dir)
+
+        pretrained_model_path = pretrain(
+            n_epochs=FLAGS.pretrain_epochs,
+            fold_idx=i
+        )
+        finetuned_model_path = finetune(
+            model_path=pretrained_model_path,
+            n_epochs=FLAGS.finetune_epochs,
+            fold_idx=i
+        )
+        print("==== fold {} done ====".format(i))
 
 
 if __name__ == "__main__":
